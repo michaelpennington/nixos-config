cbefd8849f3b9091002b74ffa97af3af1d8668a8
Michael Pennington <michaeldanielpennington@gmail.com>
2024-11-16 12:15:05 -0500

    Initial commit

---
+++ configuration.nix
@@ -0,0 +1,124 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+{ config, lib, pkgs, ... }:
+
+let libbluray = pkgs.libbluray.override {
+  withAACS = true;
+  withBDplus = true;
+  withJava = true;
+};
+vlc = pkgs.vlc.override { inherit libbluray; };
+in {
+  imports =
+    [
+      ./hardware-configuration.nix
+    ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "poseidon";
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "America/New_York";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+    useXkbConfig = true; # use xkb.options in tty.
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.mpennington = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" ]; # Enable ‘sudo’ for the user.
+  };
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  environment.systemPackages = with pkgs; [
+    alsa-utils
+    bat
+    eza
+    fd
+    gcc
+    gh
+    gnome.file-roller
+    gnome.nautilus
+    inkscape
+    lazygit
+    pavucontrol
+    playerctl
+    prismlauncher
+    ripgrep
+    rustup
+    stow
+    sway-launcher-desktop
+    swaynotificationcenter
+    vlc
+    w3m
+    wezterm
+    wget
+    wlogout
+    wob
+    xdg-user-dirs
+    zathura
+  ];
+
+  fonts.packages = with pkgs; [
+    fantasque-sans-mono
+    fira-code
+    fira-code-symbols
+    noto-fonts
+    noto-fonts-cjk
+    noto-fonts-emoji
+    (nerdfonts.override { fonts = [ "FiraCode" ]; })
+  ];
+
+  programs = {
+    firefox = {
+      enable = true;
+      languagePacks = [ "en-US" ];
+    };
+    fish.enable = true;
+    git.enable = true;
+    neovim = {
+      enable = true;
+      viAlias = true;
+      vimAlias = true;
+      defaultEditor = true;
+    };
+    starship.enable = true;
+    sway.enable = true;
+    tmux.enable = true;
+    waybar.enable = true;
+  };
+
+  xdg.portal.wlr.enable = true;
+
+  services.openssh.enable = true;
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "24.05"; # Did you read the comment?
+
+}
+
