84503e7ed3dcb97a22fdba91f6aea63a30a3d198
Michael Pennington <michaeldanielpennington@gmail.com>
2024-11-16 21:27:13 -0500

    Add homemanager

--- flake.nix
+++ flake.nix
@@ -4,6 +4,10 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     prismlauncher.url = "github:PrismLauncher/PrismLauncher/develop";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
@@ -12,6 +16,12 @@
       specialArgs = { inherit inputs; };
       modules = [
         ./configuration.nix
+	home-manager.nixosModules.home-manager {
+          home-manager.useGlobalPkgs = true;
+	  home-manager.useUserPackages = true;
+
+	  home-manager.users.mpennington = import ./home.nix;
+	}
       ];
     };
   };
---
+++ home.nix
@@ -0,0 +1,5 @@
+{ config, pkgs, ... }:
+{
+  home.username = "mpennington";
+  home.homeDirectory = "/home/mpennington";
+}
