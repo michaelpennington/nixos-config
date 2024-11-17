3a54a3dffb45f25dd56d24535370dfe3dff706fe
Michael Pennington <michaeldanielpennington@gmail.com>
2024-11-16 21:32:41 -0500

    Add starship

--- home.nix
+++ home.nix
@@ -2,4 +2,17 @@
 {
   home.username = "mpennington";
   home.homeDirectory = "/home/mpennington";
+
+  home.stateVersion = "24.05";
+
+  programs.starship = {
+    enable = true;
+    settings = {
+      time.disabled = false;
+      time.use_12hr = true;
+      time.style = "'#cfae71'";
+    };
+  };
+
+  programs.home-manager.enable = true;
 }
