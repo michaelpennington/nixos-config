e7c74330796a8cf55b92239f7a1e4aa874185291
Michael Pennington <michaeldanielpennington@gmail.com>
2025-02-11 17:29:44 -0500

    Mc Server:: update dh

--- configuration.nix
+++ configuration.nix
@@ -69,7 +69,7 @@ in {
           inherit (inputs.nix-minecraft.lib) collectFilesAt;
           modpack = pkgs.fetchPackwizModpack {
             url = "https://github.com/michaelpennington/server_mods/raw/refs/heads/main/pack.toml";
-            packHash = "sha256-W+lfnPMVt6VsXn2ZPEtYzk7Mb3cavdjBI43JVn6nHTI=";
+            packHash = "sha256-ueWDBMAceG0TP5ALs7bhhcGhxjSAA/+76ib+RvpTbX8=";
           };
           mcVersion = modpack.manifest.versions.minecraft;
           fabricVersion = modpack.manifest.versions.fabric;
