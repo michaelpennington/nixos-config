585ee35600e04e3bac6cb6205664ddf2377a8c22
Michael Pennington <michaeldanielpennington@gmail.com>
2025-02-21 10:50:22 -0500

    Add nvim mini, pairs + comments + starter

--- configuration.nix
+++ configuration.nix
@@ -222,11 +222,63 @@ in {
             ];
           };
         };
+        mini = {
+          enable = true;
+          modules = {
+            comment = {
+              mappings = {
+                comment = "<leader>/";
+                comment_line = "<leader>/";
+                comment_visual = "<leader>/";
+                textobject = "<leader>/";
+              };
+            };
+            starter = {
+              content_hooks = {
+                "__unkeyed-1.adding_bullet" = {
+                  __raw = "require('mini.starter').gen_hook.adding_bullet()";
+                };
+                "__unkeyed-2.indexing" = {
+                  __raw = "require('mini.starter').gen_hook.indexing('all', { 'Builtin actions' })";
+                };
+                "__unkeyed-3.padding" = {
+                  __raw = "require('mini.starter').gen_hook.aligning('center', 'center')";
+                };
+              };
+              evaluate_single = true;
+              header = ''
+                ███╗   ██╗██╗██╗  ██╗██╗   ██╗██╗███╗   ███╗
+                ████╗  ██║██║╚██╗██╔╝██║   ██║██║████╗ ████║
+                ██╔██╗ ██║██║ ╚███╔╝ ██║   ██║██║██╔████╔██║
+                ██║╚██╗██║██║ ██╔██╗ ╚██╗ ██╔╝██║██║╚██╔╝██║
+                ██║ ╚████║██║██╔╝ ██╗ ╚████╔╝ ██║██║ ╚═╝ ██║
+              '';
+              items = {
+                "__unkeyed-1.buildtin_actions" = {
+                  __raw = "require('mini.starter').sections.builtin_actions()";
+                };
+                "__unkeyed-2.recent_files_current_directory" = {
+                  __raw = "require('mini.starter').sections.recent_files(10, false)";
+                };
+                "__unkeyed-3.recent_files" = {
+                  __raw = "require('mini.starter').sections.recent_files(10, true)";
+                };
+                "__unkeyed-4.sessions" = {
+                  __raw = "require('mini.starter').sections.sessions(5, true)";
+                };
+              };
+            };
+            ai = {
+              n_lines = 50;
+              search_method = "cover_or_next";
+            };
+            pairs = {};
+          };
+        };
         lualine = {
           enable = true;
           settings.options.theme = "zenburn";
         };
-        comment.enable = true;
       };
       keymaps = [
         {
