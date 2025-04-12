bc1ba62c91ea8cbda2483e4e51594b0369b9b7d5
Michael Pennington <michaeldanielpennington@gmail.com>
2025-04-12 11:18:57 -0400

    AUto launch dap ui

--- configuration.nix
+++ configuration.nix
@@ -260,7 +260,27 @@
           enable = true;
           settings.codelldb_path = "${pkgs.vscode-extensions.vadimcn.vscode-lldb}/share/vscode/extensions/vadimcn.vscode-lldb/adapter/codelldb";
         };
-        dap-ui.enable = true;
+        dap-ui = {
+          enable = true;
+          luaConfig.post =
+            config.lib.nixvim.mkRaw
+            # Lua
+            ''
+              local dap, dapui = require("dap"), require("dapui")
+              dap.listeners.before.attach.dapui_config = function()
+                dapui.open()
+              end
+              dap.listeners.before.launch.dapui_config = function()
+                dapui.open()
+              end
+              dap.listeners.before.event_terminated.dapui_config = function()
+                dapui.close()
+              end
+              dap.listeners.before.event_exited.dapui_config = function()
+                dapui.close()
+              end
+            '';
+        };
         dap-virtual-text.enable = true;
         mini = {
           enable = true;
