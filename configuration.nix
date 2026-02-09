001544579de241cff966763262d5ebb91cb637b6
Michael Pennington <michaeldanielpennington@gmail.com>
2026-02-09 11:12:31 -0500

    fix renamed packages

--- configuration.nix
+++ configuration.nix
@@ -382,13 +382,13 @@ in {
         gtk3
         pango
         cairo
-        xorg.libX11
-        xorg.libXcomposite
-        xorg.libXdamage
-        xorg.libXext
-        xorg.libXfixes
-        xorg.libXrandr
-        xorg.libxcb
+        libx11
+        libxcomposite
+        libxdamage
+        libxext
+        libxfixes
+        libxrandr
+        libxcb
         libxkbcommon
         alsa-lib
         libgbm
